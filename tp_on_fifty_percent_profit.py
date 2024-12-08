ac7d2f0cb21da89b8789178ecaeb3f9404a420a9
unknown <afiqakimy123@gmail.com>
2024-12-09 03:53:28 +0800

    add another file

---
+++ tp_on_fifty_percent_profit.py
@@ -0,0 +1,21 @@
+# 1. PROFIT = 50% => TP = 100%
+
+a = 0.5 # 50%
+b = 1  # 75%
+
+capital = 50
+fee = 0.35
+
+txns = (capital - 0.35)
+fifty_percent_profit = round(txns + (txns * a), 2)
+sell_half_fifty_percent_profit = round(txns + (txns * a),2) / 2
+
+
+print(f"Capital: ${capital}")
+print(f"Buy Transaction: ${txns}")
+print("-----------------------")
+
+print(f"🟢 50% Profit: ${fifty_percent_profit}")
+print("-----------------------")
+print(f"Total Wallet: ${fifty_percent_profit}")
+print(f"Total Fee: ${fee}")
